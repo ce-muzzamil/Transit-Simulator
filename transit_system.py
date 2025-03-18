607a7d8ce2d226b3a0bd3270f8697868a6467bf5
ce-muzzamil <ce.muzzamil@gmail.com>
2025-03-18 12:05:18 +0300

    added typehints and docstrings in transit_system file

--- transit_system.py
+++ transit_system.py
@@ -1,16 +1,22 @@
 from __init__ import *
-from topology import Topology
-from bus import Bus
 
 class TransitSystem:
+    "Representation of a public transit system"
     def __init__(self,
-                 num_busses_per_route=1,
-                 min_bus_capacity=100, 
-                 max_bus_capacity=200,
-                 avg_bus_speed=16.67,
-                 seed=0
+                 num_busses_per_route: int = 1,
+                 min_bus_capacity: int = 100, 
+                 max_bus_capacity: int = 200,
+                 avg_bus_speed: float = 16.67,
+                 seed: int = 0
                  ) -> None:
-        
+        """
+        Argument:
+        --------
+        `num_busses_per_route` is a fixed max number busses per route
+        `[min,max]_bus_capacity` is the [minimum, maximum] number of people a single bus can hold
+        `avg_bus_speed`is the speed in m/s
+        `seed`: is for generating a random scenerio
+        """
         np.random.seed(seed=seed)
         self.topology = Topology()
 
@@ -34,7 +40,15 @@ class TransitSystem:
     
         self.buses = sum(self.buses, [])
         
-    def step(self, time):
+    def step(self, time) ->None:
+        """
+        It calls the step of nodes and busses
+
+        Argument:
+        --------
+        `time`: is the time is seconds starting from the first hour of the opperation to the last hour of opperation
+
+        """
         od_mat = self.topology.get_od_mat_for_time(time)
         for i, node in enumerate(self.topology.nodes):
             node.step(time, to_depart=od_mat[i, :], all_nodes=self.topology.nodes)

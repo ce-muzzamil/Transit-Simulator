aa9e9e55f2eb8d03297d4cc44a24e0c58bb1db2c
ce-muzzamil <ce.muzzamil@gmail.com>
2025-05-15 01:15:03 +0300

    ray failed

--- env.py
+++ env.py
@@ -97,9 +97,6 @@ class TransitNetworkEnv(MultiAgentEnv):
                 ),
             }
         )
-        
-        # self.osp = get_obs_space()
-        # self.asp = get_action_space()
 
         self.observation_spaces = {key: get_obs_space() for key in self.agents}
         self.action_spaces = {key: get_action_space() for key in self.agents}

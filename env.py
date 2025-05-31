0df6acd9f3402706718387f0b7a69b42ffaff90f
ce-muzzamil <ce.muzzamil@gmail.com>
2025-05-31 17:55:17 +0300

    custom ppo

--- env.py
+++ env.py
@@ -462,7 +462,10 @@ class TransitNetworkEnv:
             reward = reward_3 + reward_2
 
             buses = [bus for bus in self.transit_system.retired_buses if bus.service_route == route_id and bus.reversed == is_reversed]
-            # reward += sum([bus.num_passengers_served > 0 for bus in buses])
+            utilzed_bus_capacity = sum([bus.num_passengers_served > 0 for bus in buses])/sum([bus.capacity > 0 for bus in buses])
+
+            reward += utilzed_bus_capacity
+            
             for bus in self.transit_system.retired_buses:
                 self.transit_system.retired_buses.remove(bus)
 

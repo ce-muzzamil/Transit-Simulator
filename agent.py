7b0aab435334edeefa66324e3a25a7ce1c58a6ff
ce-muzzamil <ce.muzzamil@gmail.com>
2025-06-05 20:38:25 +0300

    custom ppo

--- agent.py
+++ agent.py
@@ -607,11 +607,19 @@ def ppo_update(
             returns_imm.insert(0, gae_imm + value_buf[agent_id][t][0])
 
             next_value_del = 0.0 if t == T - 1 else value_buf[agent_id][t + 1][1]
-            delta_del = (
-                info_buf[agent_id][t]["reward_type_2"]
-                + gamma_del * next_value_del * next_non_terminal
-                - value_buf[agent_id][t][1]
-            )
+            
+            if action_buf[agent_id][t] == 0:
+                delta_del = (
+                    info_buf[agent_id][t]["reward_type_2"]
+                    + gamma_del * next_value_del * next_non_terminal
+                    - value_buf[agent_id][t][1]
+                )
+            else:
+                delta_del = (
+                    info_buf[agent_id][t]["reward_type_2"]/1000
+                    + gamma_del * next_value_del * next_non_terminal
+                    - value_buf[agent_id][t][1]
+                )
             gae_del = delta_del + gamma_del * lam * next_non_terminal * gae_del
             advs_del.insert(0, gae_del)
             returns_del.insert(0, gae_del + value_buf[agent_id][t][1])

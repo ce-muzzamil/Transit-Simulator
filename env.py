8acf49f4a4a87294dd44520e284b0df072d5d30d
ce-muzzamil <ce.muzzamil@gmail.com>
2025-06-11 18:28:11 +0300

    custom ppo

--- env.py
+++ env.py
@@ -452,7 +452,7 @@ class TransitNetworkEnv:
                 avg_stranding_count = 0  # counts
 
             reward_2 = 0
-            reward_2 -= avg_waiting_time / 90
+            reward_2 -= avg_waiting_time / 5
 
             # if avg_stranding_count > 0 and action == 0:
             #     reward_2 += -1

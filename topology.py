7cc23f71a0c6ed39ec3481ac130b5275774036eb
ce-muzzamil <ce.muzzamil@gmail.com>
2025-07-03 23:30:57 +0300

    custom ppo

--- topology.py
+++ topology.py
@@ -329,10 +329,7 @@ class Topology:
         mask = np.isnan(y)
         y[mask] = np.linspace(0.1, y[~mask][0], mask.sum())
 
-        if np.random.rand() > 0.5:
-            return y
-        else:
-            return y #* 0.0 + 1.0
+        return y
 
     def initiallize_traffic_data(self) -> None:
         """
